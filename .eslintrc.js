9d29d206169d246fd2b0629a1a583e5cbd9b6b45
RenanSantos7 <renansantos7@proton.me>
2024-10-28 15:02:30 -0300

    :tada: meu primeiro commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: [
+    'react-refresh/only-export-components': [
+    'warn',
+    { allowConstantExport: true },
+  ],
+'indent': ['error', 'tab' ],
+'sort-imports': [
+	'error',
+	{
+		'ignoreCase': true,
+		'ignoreDeclarationSort': false,
+		'ignoreMemberSort': false,
+		'memberSyntaxSortOrder': ['none', 'all', 'single', 'multiple'],
+	}
+],
+  ]
 };
